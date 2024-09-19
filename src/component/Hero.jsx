89d02665b84ec34605d421d4249bbdc52069c2ba
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-19 18:15:00 -0500

    fixed size images

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () =>{
   return(
     <>
       <picture>
-        <img className="w-[100%] h-200 object-cover rounded-t-[10px] md:rounded-l-[10px] md:rounded-r-none md:w-285 md:h-[100%]" src="./images/drawers.jpg" alt="Green cabinet with drawers and a decorative vase filled with dried flowers on the surface" />
+        <img className="w-[100%] h-200 object-cover rounded-t-[10px] md:rounded-l-[10px] md:rounded-r-none md:w-285 md:h-[100%]" src="./images/drawers.webp" alt="Green cabinet with drawers and a decorative vase filled with dried flowers on the surface" />
       </picture>
       
     </>
--- src/component/ImageProfile.jsx
+++ src/component/ImageProfile.jsx
@@ -2,7 +2,7 @@ const ImageProfile = () =>{
   return(
     <>
      <picture>
-        <img className="w-10 h-10 rounded-full" src="./images/avatar-michelle.jpg" alt="Woman with long hair and scarf, standing in front of a body of water, with a serene and contemplative expression" />
+        <img className="w-10 h-10 rounded-full" src="./images/avatar-michelle.webp" alt="Woman with long hair and scarf, standing in front of a body of water, with a serene and contemplative expression" />
       </picture>
     </>
   )
