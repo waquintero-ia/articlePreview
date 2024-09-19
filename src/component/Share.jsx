8a60d7829512e24382f08a6af154a7bdfd590db7
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-09-19 18:31:24 -0500

    fixed buttoms aria-label

--- src/component/Share.jsx
+++ src/component/Share.jsx
@@ -4,7 +4,7 @@ const Share = ({handleClick, share}) =>{
   
   return(
     <>
-      <button className={`w-8 h-8 flex justify-center items-center bg-blue-300 rounded-full ${share && 'md:bg-blue-800 md:text-white'}`} onClick={handleClick}>
+      <button className={`w-8 h-8 flex justify-center items-center bg-blue-300 rounded-full ${share && 'md:bg-blue-800 md:text-white'}`} onClick={handleClick} aria-label='share'>
         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
           <path fill="currentColor" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/>
         </svg>      
--- src/component/SharePopup.jsx
+++ src/component/SharePopup.jsx
@@ -28,7 +28,7 @@ const SharePopup = ({handleClick}) =>{
             </a>
             
           </div>
-          <button className="share w-8 h-8 bg-blue-800 rounded-full flex justify-center items-center text-white md:hidden focus:bg-inherit">
+          <button className="share w-8 h-8 bg-blue-800 rounded-full flex justify-center items-center text-white md:hidden focus:bg-inherit" aria-label='share'>
             <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13">
               <path fill="currentColor" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/>
             </svg>
